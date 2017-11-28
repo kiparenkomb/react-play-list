0885e2e202e1c1bd6803f867b95d8a51615af5fa
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2017-11-28 12:13:45 +0200

    Created button play

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+function PlayButton(props) {
+    const className = props.isMusicPlaying ? 'play is-active' : 'play';
+    return <a
+                onClick={props.onClick}
+                href="#" title="Play video" className={className}
+            />;
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { isMusicPlaying: false };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    // handleClick(){
+    //     if (this.state.isMusicPlaying) {
+    //         this.setState({ isMusicPlaying: false });
+    //     } else {
+    //         this.setState({ isMusicPlaying: true });
+    //     }
+    // }
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                isMusicPlaying: !prevState.isMusicPlaying
+            };
+        });
+    };
+    render() {
+        const status = this.state.isMusicPlaying ? 'Playing' : 'Not playing';
+        return (
+            <div>
+                <h1 onClick={this.handleClick}>{ status }</h1>
+                <PlayButton
+                    onClick={this.handleClick}
+                    isMusicPlaying={this.state.isMusicPlaying}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
