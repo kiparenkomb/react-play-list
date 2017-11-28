99f12aee0a4dc2e24164e19e3339e847fb410846
nkiparenko <nkiparenko@corp.web4pro.com.ua>
2017-11-28 18:07:36 +0200

    Created audio download player

--- src/App.js
+++ src/App.js
@@ -2,33 +2,70 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+
 function PlayButton(props) {
     const className = props.isMusicPlaying ? 'play is-active' : 'play';
+    const titleName = props.isMusicPlaying ? 'Stop music' : 'Play music';
     return <a
                 onClick={props.onClick}
-                href="#" title="Play video" className={className}
+                href="#"
+                title={titleName}
+                className={className}
             />;
 }
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { isMusicPlaying: false };
+        this.state = {
+            isMusicPlaying: false,
+            autoPlayAudio: true,
+            loopAudio: false,
+            srcAudio : ''
+        };
         this.handleClick = this.handleClick.bind(this);
+        this.handleOnFileUpload = this.handleOnFileUpload.bind(this);
     }
-    // handleClick(){
-    //     if (this.state.isMusicPlaying) {
-    //         this.setState({ isMusicPlaying: false });
-    //     } else {
-    //         this.setState({ isMusicPlaying: true });
-    //     }
-    // }
-    handleClick() {
-        this.setState(prevState => {
-            return {
-                isMusicPlaying: !prevState.isMusicPlaying
-            };
-        });
+    handleOnFileUpload(e) {
+        const { files } = e.target;
+        // console.log(files);
+        if (files.length === 0) {
+            this.setState({
+                srcAudio: '',
+                isMusicPlaying: false
+            });
+            // console.log('target none');
+            return false;
+        } else {
+            this.setState({
+                srcAudio: URL.createObjectURL(files[0]),
+                isMusicPlaying: true
+            });
+        }
+        // this.audio.onended = () => {
+        this.audio.onended = () => {
+            this.setState({
+                isMusicPlaying: false
+            });
+        };
+
+    }
+    handleClick(event) {
+        event.preventDefault();
+        if (this.state.srcAudio.length) {
+            if (this.state.isMusicPlaying) {
+                this.audio.pause();
+            } else {
+                this.audio.play();
+            }
+            this.setState(prevState => {
+                return {
+                    isMusicPlaying: !prevState.isMusicPlaying
+                };
+            });
+        } else {
+            return false;
+        }
     };
     render() {
         const status = this.state.isMusicPlaying ? 'Playing' : 'Not playing';
@@ -39,6 +76,17 @@ class App extends Component {
                     onClick={this.handleClick}
                     isMusicPlaying={this.state.isMusicPlaying}
                 />
+                <h1>Play Music</h1>
+                <input
+                    ref="file"
+                    type="file"
+                    onChange={this.handleOnFileUpload} />
+                <audio
+                    id="audio"
+                    src={ this.state.srcAudio }
+                    ref={(audioTag) => { this.audio = audioTag }}
+                    autoPlay={ this.state.autoPlayAudio }
+                    loop={ this.state.loopAudio } />
             </div>
         );
     }
